f5b4e5ff8f559abe74d97890b9673e4728ca70b8
João Elias Maccari <joaomaccari@hotmail.com>
2022-05-19 06:30:05 -0300

    att css

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 const pessoas = [];
-var input = document.getElementById("enterInput");
+let input = document.getElementById("enterInput");
 let lista = document.getElementById("lista");
 
 function addPessoa() {
-    let pessoa = input.value;
+    let pessoa = String(input.value);
+
     //insere o input dentro de um arrey
     if (pessoa == "") {
         alert("Insira um nome")
@@ -12,7 +13,7 @@ function addPessoa() {
         pessoas.push(pessoa)
     
         input.value = ""
-    
+        
         mostraLista();
         
     }
@@ -21,33 +22,51 @@ function addPessoa() {
 
 function mostraLista() {
         
-        lista.innerHTML = " "
-        for (index of pessoas) {
-        //criar item em um botao que esclui o nome da lista
-        //nomme
+        lista.innerHTML = ""
+        for (i in pessoas) {
+        
+        //crio um li para a ul
         let item = document.createElement("li");
         item.setAttribute("class", "list-item")
-        item.appendChild(document.createTextNode(index))
         
+        //crio uma variavel que recebe o index
+        var pessoa = document.createTextNode(pessoas[i])
+        item.appendChild(pessoa)
         lista.appendChild(item);
         //botao de delete
     
         let btnDel = document.createElement("button");
-        btnDel.setAttribute("onclick", `deletaNome(${input})`);
         btnDel.appendChild(document.createTextNode("x"));
+        btnDel.setAttribute( "onclick", `deletaNome(${i})`);
+        btnDel.setAttribute("id", "del-btn")
         item.appendChild(btnDel);
         
             
         }
-
+            
 }
 
 function deletaNome (pos) {
 
-    pessoas.splice(pos, 0)
+    pessoas.splice(pos, 1)
+    mostraLista()
 }
 
 function sortear () {
-    //sortear um dos nomes cadastrados 
+    var divSorteado = document.getElementsByClassName("div-sorteado")[0]
+    
+    //sortear um dos nomes cadastrados
+    let sorteados = pessoas.length
+    var ale = Math.floor(Math.random(pessoas) * sorteados);
+    for (i in pessoas) {
+        
+        var sortudo = pessoas[ale]
+        
+        divSorteado.innerHTML = `<p>O sorteado foi ${sortudo}<p>`
+        
+    }
+
+    
+
 }
 
